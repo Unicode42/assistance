c160c76d0cac53eca3d14fd3c4600c228fd1f3f2
Kistler Wojtek <kistler.wojtek1919@gmail.com>
2020-11-14 17:04:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web'
+import NewsCards from './components/NewsCards/NewsCards'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import useStyles from './styles'
+import logo from './images/logo.png'
+import { Typography, Modal, Card } from '@material-ui/core'
+import wordsToNumbers from 'words-to-numbers'
+
+
+
+const alanKey = '07df128ff818da963e72b6767ae4a7c32e956eca572e1d8b807a3e2338fdd0dc/stage'
+
+const App = () => {
+
+    const classes = useStyles()
+
+    const [activeArticle, setActiveArticle] = useState(0)
+    const [newsArticles, setNewsArticles] = useState([])
+    const [isOpen, setIsOpen] = useState(false)
+
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles, number }) => {
+                if (command === 'newHeadlines') {
+                    setNewsArticles(articles);
+                    setActiveArticle(-1);
+                } else if (command === 'instructions') {
+                    setIsOpen(true);
+                } else if (command === 'highlight') {
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+                } else if (command === 'open') {
+                    const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+                    const article = articles[parsedNumber - 1];
+
+                    if (parsedNumber > 20) {
+                        alanBtn().playText('Please try that again...');
+                    } else if (article) {
+                        window.open(article.url, '_blank');
+                        alanBtn().playText('Opening...');
+                    } else {
+                        alanBtn().playText('Please try that again...');
+                    }
+                }
+            },
+        });
+    }, []);
+
+    return (
+        <div>
+            <div className={classes.logoContainer}>
+                {newsArticles.length ? (
+                    <div className={classes.infoContainer}>
+                        <Card className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [3]</Typography></Card>
+                        <Card className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></Card>
+                    </div>
+                ) : null}
+                <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="logo" />
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+            <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+            {!newsArticles.length ? (
+                <div className={classes.footer}>
+                    <Typography variant="body1" component="h2">
+                        Created by
+            <a className={classes.link} href="https://twitter.com/kistler1919"> Kistler Wojtek</a> -
+           
+                    </Typography>
+                    <img className={classes.image} src={logo} height="50px" alt="JSMastery logo" />
+                </div>
+            ) : null}
+        </div>
+    )
 }
 
-export default App;
+export default App
